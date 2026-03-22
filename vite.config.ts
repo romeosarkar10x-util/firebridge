b93ca302eac3b8ac8e79a92b786e4fd08684e146
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-03-22 19:52:41 +0530

    Updated viteconfig to work properly when deploying to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
+
+function getBaseURL(): string {
+    const githubRepository = process.env.GITHUB_REPOSITORY;
+
+    if (githubRepository === undefined) {
+        return "/";
+    }
+
+    const [, repo] = githubRepository.split("/");
+    return `/${repo}`;
+}
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: getBaseURL(),
+
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
 });
