1ad141338064b773a34c23f789c8704eb99e1554
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-03-22 20:25:46 +0530

    Removed login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import "./App.css";
-import { LoginForm } from "./components/LoginForm";
 
 function App() {
-    return (
-        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-            <div className="w-full max-w-sm md:max-w-4xl">
-                <LoginForm />
-            </div>
-        </div>
-    );
+    return <p>Hello world!</p>;
 }
 
 export default App;
